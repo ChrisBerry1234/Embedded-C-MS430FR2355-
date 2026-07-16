98841bd33ecbe4b5ee9115d93d865bb075e67b2b
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-07-16 17:38:06 -0400

    Update immediate values and comments in ASM file

--- basics/Assembly/ASM_AddrMode2_Immediate.asm
+++ basics/Assembly/ASM_AddrMode2_Immediate.asm
@@ -6,7 +6,7 @@
 mov.w   #100, R4          ; R4 = 100
 
 ; Load hexadecimal constant
-mov.w   #0x1234, R5       ; R5 = 0x1234
+mov.w   #1234h, R5       ; R5 = 0x1234
 
 ; Load maximum unsigned byte
 mov.b   #0xFF, R6         ; Lower byte of R6 = 0xFF
@@ -14,8 +14,13 @@ mov.b   #0xFF, R6         ; Lower byte of R6 = 0xFF
 ; Load binary value
 mov.b   #0b10101010, R7   ; R7 = 0xAA
 
+; Load Character into 'B', R10    
+mov.b   #'B', R10         ; put 'B' into R10
+
 ; Load zero
 mov.w   #0, R8            ; Clear R8 using immediate value
 
 ; Load negative number
 mov.w   #-25, R9          ; Two's complement representation
+
+jmp     main
