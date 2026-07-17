9e313383fdfa54eabea3981581e5d1e63d5732a2
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-07-16 23:08:58 -0400

    Implement copying of constants to registers and variables
    Added instructions to copy constants into registers and store them in variables.

--- basics/Assembly/Asm_AddrMode4_Symbolic.asm
+++ basics/Assembly/Asm_AddrMode4_Symbolic.asm
@@ -3,7 +3,13 @@
 ;------------------------------------
 
 main: 
-          jmp main
+          mov.w    Const1, R4  ; copy contents of Const1 into Register R4
+          mov.w    R4, Var1    ; copy all contents of R4 into Var1
+
+          mov.w    Const2, R5  ; copy contents of Const2 into Register R5
+          mov.w    R5, Var2    ; copy all contents of R5 into Var2
+
+          jmp      main
 
 
 ;-----------------------------------
@@ -12,9 +18,9 @@ main:
           .data               ; go to data memory
           .retain             ; DO NOT OPTIMIZE
 
-Const1:    .short  1234h      ;
-Const2:    .short  0CAFEh
+Const1:   .short    1234h       ; 1234h @2000h
+Const2:   .short    0CAFEh      ; 0CAFEh @2002h
 
-Var1:      .space   2
-Var2:      .space   2
+Var1:     .space     2          ; reserve 2 bytes @ 2004h 
+Var2:     .space     2          ; reserve 2 bytes @ 2006h
 
