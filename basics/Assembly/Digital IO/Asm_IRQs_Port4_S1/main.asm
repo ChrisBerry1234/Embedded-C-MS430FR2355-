1e4129019338dee8d13f0d2a4fefe9a6b2b0625c
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-07-27 22:13:45 -0400

    Update main.asm

--- basics/Assembly/Digital IO/Asm_IRQs_Port4_S1/main.asm
+++ basics/Assembly/Digital IO/Asm_IRQs_Port4_S1/main.asm
@@ -1,1 +1,29 @@
+;---------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-----------------------------------------------------
+            .cdecls C, LIST, "msp430.h"                 ; Include device header file
+;------------------------------------------------------
+            .def    RESET                               ; Export program entry point to make it known to linker
+;------------------------------------------------------
+            .text                                       ; Assemble into program memory
+            .retain                                     ; Override ELF conditional linking
+                                                        ; and retain current section
+            .retainrefs                                 ; And retain any sections that have
+                                                        ; references to current section
+;-------------------------------------------------------
+RESET       mov.w    #__STACK_END, SP                   ; Initialize stackpointer
+StopWDT     mov.w    #WDTPW|WDTHOLD, &WDTCTL            ; Stop watchdog timer
 
+;-------------------------------------------------------
+; Main Loop Here
+;-------------------------------------------------------
+;--------------------------------------------------------
+; Data Allocation
+;---------------------------------------------------------
+;--------------------------------------------------------
+; Stack Pointer Definition
+;--------------------------------------------------------
+            .global    ___STACK_END
+            .sect      .stack 
