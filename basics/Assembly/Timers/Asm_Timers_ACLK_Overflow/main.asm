6bf903bb887ab4f36a2aab967e595732ba71afe4
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-07-29 22:57:35 -0400

    Add MSP430 timer overflow interrupt assembly code
    This assembly code sets up a timer overflow interrupt for the MSP430, initializes the stack pointer, and configures an LED.

---
+++ basics/Assembly/Timers/Asm_Timers_ACLK_Overflow/main.asm
@@ -0,0 +1,58 @@
+;---------------------------------------------------
+; MSP430 Assembler Code Template for use with TI Code Composer Studio
+;
+;
+;-----------------------------------------------------
+            .cdecls C, LIST, "msp430.h"                 ; Include device header file
+;------------------------------------------------------
+            .def    RESET                               ; Export program entry point to make it known to linker
+;------------------------------------------------------
+            .text                                       ; Assemble into program memory
+            .retain                                     ; Override ELF conditional linking
+                                                        ; and retain current section
+            .retainrefs                                 ; And retain any sections that have
+                                                        ; references to current section
+;-------------------------------------------------------
+RESET       mov.w    #__STACK_END, SP                   ; Initialize stackpointer
+StopWDT     mov.w    #WDTPW|WDTHOLD, &WDTCTL            ; Stop watchdog timer
+
+;-------------------------------------------------------
+; Main Loop Here
+;-------------------------------------------------------
+init:
+;----setup LED initially
+            bis.b    #BIT0, &P1DIR
+            bic.b    #BIT0, &P1OUT
+            bic.b    #LOCKLPM5, &PM5CTL0                 ; enabled digit I/O
+
+;----timer setup B0
+            bis.w    #TBCLR, &TB0CTL                     ; clear TB0
+            bis.w    #TBSSEL__ACLK, &TB0CTL              ; choose ACLK
+            bis.w    #MC__CONTINUOUS, &TB0CTL            ; put into continuous mode
+
+;-----timer overflow IRQ setup
+            bis.w    #TBIE,  &TB0CTL                     ; local enable for TB0 overflow
+            eint                                         ; enable global interrupt for all maskable
+            bic.w    #TBIFG, &TB0CTL                     ; clear Interrupt flag for IRQ assertion
+
+main:        
+            jmp      main
+            
+;--------------------------------------------------------
+; Data Allocation
+;---------------------------------------------------------
+;--------------------------------------------------------
+; Interrupt Service Routines
+;---------------------------------------------------------
+ISR_TB0_Overflow:
+            xor.b       #BIT0, &P1OUT                         ; toggle LED1
+            bic.b       #TBIFG, &TB0CTL                       ; clear P4IFG otherwise interrupt will continue to run
+            reti
+;--------------------------------------------------------
+; Stack Pointer Definition
+;--------------------------------------------------------
+            .global    ___STACK_END
+            .sect      .stack 
+
+            .sect     ".int42"                                 ; init vector table for TB0 overflow
+            .short    ISR_TB0_Overflow 
