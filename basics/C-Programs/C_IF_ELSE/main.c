26a1d84a087a16f46765ddad8dcaadc5565e450f
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-08-05 14:01:07 -0400

    Implement check for i equal to 2 in loop
    Added logic to set var_is_two based on the value of i in the loop.

--- basics/C-Programs/C_IF_ELSE/main.c
+++ basics/C-Programs/C_IF_ELSE/main.c
@@ -5,5 +5,21 @@ int main(void)
   WDCTL = WDTPW | WDTHOLD;
 
   int i = 0;
-  int
+  int var_is_two = 0;
+
+  while(1)
+  {
+    for(i=0; i<5; i=i+1)
+      {
+        if(i == 2)
+        {
+          var_is_two = 1;
+        }
+
+        else
+        {
+          var_is_two = 0;
+        }
+      }
+  }
 }
