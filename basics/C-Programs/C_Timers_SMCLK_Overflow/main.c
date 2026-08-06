39e8b48f04cb5fb97e4a1c46a279c59b0be06b5b
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-08-06 19:21:14 -0400

    Create main.c

---
+++ basics/C-Programs/C_Timers_SMCLK_Overflow/main.c
@@ -0,0 +1,43 @@
+#include <msp430.h>
+
+int main(void)
+{
+  WDTCTL = WDTPW | WDTHOLD;  //stop watchdog timer
+
+  //setup ports 
+  P1DIR |= BIT0;          //P1.0 to an output
+  P1OUT &= ~BIT0;        //set P1.0 LED off
+
+  P6DIR |= BIT0;           //Enable P6 as an Output
+  P6OUT |= BIT0;           //Turning LED On 
+
+  PM5CTL0 &= ~LOCKLPM5;    //enable digital I/O
+
+  //-Setup Timer
+  TB0CTL |= TBCLR;          // clear TB0 timer
+  TB0CTL |= TBSSEL__SMCLK;  // choose ACLK as main clock source
+  TB0CTL |= MC__CONTINUOUS; // Put timer into continous mode
+
+  //setup timer IRQ
+  TB0CTL |= TBIE;           // set local enable for interrupts
+  __enable_interrupt();     // enable all maskable IRQs
+  TB0CTL &= ~ TBIFG;        // clear interrupt flag for interrupt assert
+
+  //Main Loop
+  while(1)
+  {};
+  
+
+  return 0;
+}
+
+//---------ISRs---------------//
+#pragma vector = TIMER0_B1_VECTOR 
+#interrupt void ISR_TB0_Overflow(void)
+{
+  P1OUT ^= BIT0; //TOGGLE LED ON
+  P6OUT ^= BIT0; //TOGGLE LED OFF
+  
+  TBOCTL &= ~TBIFG;
+}
+}
