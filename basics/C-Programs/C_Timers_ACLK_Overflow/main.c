87af6bf45ddff3b7dfeabbc7e80c37b84bb73d94
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-08-06 15:06:30 -0400

    Update main.c

--- basics/C-Programs/C_Timers_ACLK_Overflow/main.c
+++ basics/C-Programs/C_Timers_ACLK_Overflow/main.c
@@ -1,1 +1,35 @@
+#include <msp430.h>
 
+int main(void)
+{
+  WDTCTL = WDTPW | WDTHOLD;  //stop watchdog timer
+
+  //setup ports 
+  P1DIR |= BIT0;          //P1.0 to an output
+  P1OUT &= ~BIT0;        //set P1.0 LED off
+
+  PM5CTL0 &= ~LOCKLPM5;  //enable digital I/O
+
+  //-Setup Timer
+  TB0CTL |= TBCLR;         //clear TB0 timer
+  TB0CTL |= TBSSEL__ACLK;  // choose ACLK as main clock source
+  TB0CTL |= MC__CONTINUOUS; // Put timer into continous mode
+
+  //setup timer IRQ
+  TB0CTL |= TBIE;           // set local enable for interrupts
+  __enable_interrupt();     //enable all maskable IRQs
+  TB0CTL &= ~ TBIFG;        // Clear interrupt flag for interrupt assert
+
+  //Main Loop
+  while(1){};
+
+  return 0;
+}
+
+//---------ISRs---------------//
+#pragma vector = TIMER0_B1_VECTOR 
+#interrupt void ISR_TB0_Overflow(void)
+{
+  P1OUT ^= BIT0; //TOGLLE LED
+  TB0CTL &= ~TBIFG;
+}
