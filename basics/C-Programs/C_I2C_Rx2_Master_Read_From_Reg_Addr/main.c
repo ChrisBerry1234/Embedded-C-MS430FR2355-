1de8cd0aad816b3f05b74b3b6ec2d0aa00df4f53
Christopher Berry <138330487+ChrisBerry1234@users.noreply.github.com>
2026-08-20 20:10:49 -0400

    Refactor I2C configuration and error handling

--- basics/C-Programs/C_I2C_Rx2_Master_Read_From_Reg_Addr/main.c
+++ basics/C-Programs/C_I2C_Rx2_Master_Read_From_Reg_Addr/main.c
@@ -1,4 +1,5 @@
 #include <msp430.h>
+
 //======================================================
 // GLOBAL VARIABLES
 //======================================================
@@ -18,8 +19,8 @@ typedef enum
 } i2c_result_e;
 
 //APIs DECLARATION
-static void i2c_start_tx(void);
-static void i2c_start_rx(void);
+static void         i2c_start_tx(void);
+static void         i2c_start_rx(void);
 static i2c_result_e i2c_wait_stop(void);
 
 //======================================================
@@ -28,102 +29,45 @@ static i2c_result_e i2c_wait_stop(void);
 
 int main(void)
 {
-    // Stop watchdog timer
     WDTCTL = WDTPW | WDTHOLD;
 
-    //==================================================
-    // I2C CONFIGURATION
-    //==================================================
-
-    // Put eUSCI_B0 into software reset
     UCB0CTLW0 |= UCSWRST;
 
+    UCB0CTLW0 |= UCSSEL_3;   // SMCLK
+    UCB0BRW    = 10;         // 100 kHz
 
-    //----- I2C CLOCK ----------------------------------
+    UCB0CTLW0 |= UCMODE_3;   // I2C
+    UCB0CTLW0 |= UCMST;      // Master
+    UCB0I2CSA  = 0x68;       // Slave addr
 
-    // Select SMCLK
-    UCB0CTLW0 |= UCSSEL_3;
+    UCB0CTLW1 |= UCASTP_2;   // Auto STOP
+    UCB0TBCNT  = DATA_SIZE;
 
-    // SMCLK / 10 = 100 kHz
-    UCB0BRW = 10;
-
-
-    //----- I2C SYSTEM ---------------------------------
-
-    // I2C mode
-    UCB0CTLW0 |= UCMODE_3;
-
-    // Master mode
-    UCB0CTLW0 |= UCMST;
-
-    // Slave address
-    UCB0I2CSA = 0x68;
-
-
-    //----- I2C DATA TRANSMISSION ----------------------
-
-    // Automatic STOP when byte counter reaches TBCNT
-    UCB0CTLW1 |= UCASTP_2;
-
-    // Number of bytes to transmit
-    UCB0TBCNT = DATA_SIZE;
-
-
-    //==================================================
-    // PORT CONFIGURATION
-    //==================================================
-
-    // P1.3 = SCL
     P1SEL1 &= ~BIT3;
-    P1SEL0 |=  BIT3;
+    P1SEL0 |=  BIT3;         // SCL
 
-    // P1.2 = SDA
     P1SEL1 &= ~BIT2;
-    P1SEL0 |=  BIT2;
+    P1SEL0 |=  BIT2;         // SDA
 
-    // Unlock GPIO pins
     PM5CTL0 &= ~LOCKLPM5;
 
-
-    // Take eUSCI_B0 out of software reset
     UCB0CTLW0 &= ~UCSWRST;
 
-
-    //==================================================
-    // INTERRUPT CONFIGURATION
-    //==================================================
-
-    // Enable TX buffer interrupt + NACK interrupt
     UCB0IE |= UCTXIE0 | UCRXIE0;
-
-    // Enable global interrupts
     __enable_interrupt();
-
-
-    //==================================================
-    // MAIN LOOP
-    //==================================================
 
     while (1)
     {
-
         i2c_start_tx();
-        // Wait for STOP to finish before next transaction
         i2c_result_e result = i2c_wait_stop();
-        if (result == I2C_RESULT_ERROR_TIMEOUT)
-        {
-            return 1; 
-        }
+        if (result == I2C_RESULT_ERROR_TIMEOUT || result == I2C_RESULT_ERROR_RX)
+            return 1;
 
         i2c_start_rx();
-        // Wait for STOP to finish before next transaction
-        i2c_result_e result = i2c_wait_stop();
-        if (result == I2C_RESULT_ERROR_TIMEOUT)
-        {
-            return 1; 
-        }
+        result = i2c_wait_stop();
+        if (result == I2C_RESULT_ERROR_TIMEOUT || result == I2C_RESULT_ERROR_RX)
+            return 1;
 
-        // Small delay between transactions
         __delay_cycles(50000);
     }
 
@@ -134,57 +78,48 @@ int main(void)
 
 static void i2c_start_tx(void)
 {
-    UCB0CTLW0 |= UCTR;     // Transmitter mode
-    UCB0CTLW0 |= UCTXSTT; // PUT INTO START MODE
+    UCB0CTLW0 |= UCTR;      // TX mode
+    UCB0CTLW0 |= UCTXSTT;   // START
 }
 
 static void i2c_start_rx(void)
 {
-    UCB0CTLW0 &= ~UCTR;       //receiver mode
-    UCB0CTLW0 |= UCTXSTT;    // PUT INTO START MODE AND START TRANSMISSION
-     
+    UCB0CTLW0 &= ~UCTR;     // RX mode
+    UCB0CTLW0 |= UCTXSTT;   // START
 }
 
 static i2c_result_e i2c_wait_stop(void)
 {
     uint16_t retries = RETRY_COUNT;
 
-    //wait until STOP flag is asserted or timeout(AND)
-    while(!(UCB0IFG & UCSTPIFG) && 
-          !(UCB0STAT & UCNACKIFG) && --retries){}
-    //TIMEOUT
+    while ((UCB0CTLW0 & UCTXSTP) && !(UCB0IFG & UCNACKIFG) && --retries)
+    {
+    }
+
     if (retries == 0)
-    {
         return I2C_RESULT_ERROR_TIMEOUT;
-    }
 
-    if ((UCB0STAT & UCNACKIFG))
-    {
+    if (UCB0IFG & UCNACKIFG)
         return I2C_RESULT_ERROR_RX;
-    }
 
-    //CLEAR STOP FLAG
-    UCB0IFG &= ~UCSTPIFG;
-
-    return I2C_RESULT_OK;   
+    return I2C_RESULT_OK;
 }
 
-
-//-------Interrupt Service Routines
-#pragma vector EUSCI_B0_VECTOR
-__Interrupt Void EUSCI_B0_I2C_VECTOR(void)
+//-------Interrupt Service Routine-----------
+#pragma vector = EUSCI_B0_VECTOR
+__interrupt void EUSCI_B0_I2C_ISR(void)
 {
-    switch(__even_in_range(UCBIV, 0x1E))
-        {
-            case 0x16:                   // ID 16: RXIFG0
-                data_read = UCB0RXBUF;   //Read data from Rx Buffer
-                break;
+    switch (__even_in_range(UCB0IV, 0x1E))
+    {
+        case 0x14:                   // RXIFG0
+            data_read = UCB0RXBUF;
+            break;
 
-            case 0x18:                   // ID 18: TXIFG0
-                UCB0TXBUF = 0x03;        // Send Reg Addr
-                break;
+        case 0x16:                   // TXIFG0
+            UCB0TXBUF = 0x03;        // example: send reg addr
+            break;
 
-            default:
-                break;
-        }
+        default:
+            break;
+    }
 }
